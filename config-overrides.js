884767c23448d317a72bd4ea7e7ac618df2a2df8
Bullit-s <60654980+Bullit-s@users.noreply.github.com>
2020-05-12 13:57:26 +0300

    Import Ant

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,49 @@
-const { override, fixBabelImports } = require("customize-cra");
+const fs = require("fs");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WebpackShellPlugin = require("webpack-shell-plugin");
+const lessToJs = require("less-vars-to-js");
+const { useBabelRc, override } = require("customize-cra");
 
-module.exports = function override(config, env) {
-  // do stuff with the webpack config...
-  return config;
-};
-module.exports = override(
-  fixBabelImports("import", {
-    libraryName: "antd",
-    libraryDirectory: "es",
-    style: "css",
-  })
+const themeVariables = lessToJs(
+  fs.readFileSync(path.join(__dirname, "src/styles/ant-vars.less"), "utf8")
 );
+
+module.exports = (config) => {
+  config.module.rules.push({
+    test: /\.(less)$/,
+    use: [
+      MiniCssExtractPlugin.loader,
+      "css-loader",
+      {
+        loader: "less-loader",
+        options: {
+          modifyVars: themeVariables,
+          javascriptEnabled: true,
+        },
+      },
+    ],
+  });
+
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    use: [
+      {
+        loader: "astroturf/loader",
+        options: { extension: ".module.scss" },
+      },
+    ],
+  });
+
+  config.plugins.push(new MiniCssExtractPlugin());
+
+  // if (process.env.NODE_ENV !== "production" && false) {
+  //   config.plugins.push(
+  //     new WebpackShellPlugin({
+  //       onBuildEnd: ["nodemon server.js --watch server.js --watch mainRoutes"],
+  //       dev: true,
+  //     })
+  //   );
+  // }
+  return override(useBabelRc())(config);
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'antd';
+import "./styles/ant-components.less";
+import Menu from "antd/es/menu";
+import { Layout, Breadcrumb } from 'antd';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import styled from "astroturf";
 
-function App() {
+const { Content, Footer, Sider } = Layout;
+
+export const App: React.FC<{}> = () => {
+
   return (
-    <div className="App">
-      {/* <Button type="primary">Button</Button> */}
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider >
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Option 1
+            </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            Option 2
+            </Menu.Item>
+          <Menu.SubMenu key="sub1" icon={<UserOutlined />} title="User">
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </Menu.SubMenu>
+          <Menu.SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </Menu.SubMenu>
+          <Menu.Item key="9" icon={<FileOutlined />} />
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+            Bill is a cat.
+            </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
   );
 }
 
-export default App;
+
